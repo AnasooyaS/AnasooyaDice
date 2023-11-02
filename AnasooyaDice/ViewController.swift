3ae9b952b24a4f0d8e67eb4d6db3bde855fee484
AnasooyaS <sasindrananasooya@gmail.com>
2023-11-02 11:23:38 +0530

    Initial Commit

---
+++ AnasooyaDice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnasooyaDice
+//
+//  Created by Anasooya on 02/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
